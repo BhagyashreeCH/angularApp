c7ba72e0e086ce9521fdef63023b19b4c1ce00dc
BhagyaCH <bhagya.eee08@gmail.com>
2019-09-02 12:53:07 -0300

    updated json file

--- src/services/places-service.service.ts
+++ src/services/places-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IPlaceDetails } from '../model/placedetails.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,16 +9,17 @@ export class PlacesServiceService {
   
   constructor(private httpclient:HttpClient) {
     }
-  url ="http://localhost:3000/placesList";
+  //url ="http://localhost:3000/placesList";
+  geturl : string = "/assets/db.json"
   getPlaces(): Observable<IPlaceDetails[]>{
-    return this.httpclient.get<IPlaceDetails[]>("http://localhost:3000/placesList");
+    return this.httpclient.get<IPlaceDetails[]>(this.geturl);
   }
-  addplaces(data:IPlaceDetails): Observable<IPlaceDetails>{
-    return this.httpclient.post<IPlaceDetails>(this.url, data, {
-      headers: new HttpHeaders({
-        'Content-Type' : 'application/json'
-      })
-    })
+  //addplaces(data:IPlaceDetails): Observable<IPlaceDetails>{
+   // return this.httpclient.post<IPlaceDetails>(this.url, data, {
+      //headers: new HttpHeaders({
+        //'Content-Type' : 'application/json'
+      //})
+    //})
     
-  }
+  //}
 }
