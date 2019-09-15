2c72e2fb9ab9ec4354f807405a5ff617badb6c9d
BhagyaCH <bhagya.eee08@gmail.com>
2019-09-15 11:04:21 -0300

    added submenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  showSubmenu = false;
   title = 'my-poc';
   setcolor = "green";
   datafromchild;
   parentData:string = "content from parent";
   childdata(msg:string){
     this.datafromchild = msg;
+  }
+  showmenu(){
+    this.showSubmenu = !this.showSubmenu;
   }
 }
